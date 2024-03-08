16031e3b16c98fc7e1b1c1b6d9b5b428c285894c
kost90 <kostdonc90@gmail.com>
2024-03-08 15:09:10 +0000

    added new paragraps and certificates in resume page

--- app/ui/resume/Picture.tsx
+++ app/ui/resume/Picture.tsx
@@ -30,6 +30,7 @@ function Pictures() {
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5 }}
           viewport={{ once: true, amount: 0.2 }}
+          key={i}
         >
           <Image src={el} alt={`picture-${i}`} width={500} />
         </motion.div>
